4cd2740c5bbdff17ee7b5aee5d2522e655f3d133
Blucilha <blucilha.nascimento@gmail.com>
2021-10-24 18:00:19 -0300

    Throws refatorado

--- back_end/src/schemas/throwSchema.js
+++ back_end/src/schemas/throwSchema.js
@@ -7,8 +7,8 @@ const REGEX_VALUE = /^([0-9]*).([0-9]{2})/;
 const createThrow = Joi.object({
     id_provider: Joi.string().pattern(REGEX_ID).required(),
     id_offer: Joi.string().pattern(REGEX_ID).required(),
-    value: Joi.string().pattern(REGEX_VALUE).required,
-    amount: Joi.string().pattern(REGEX_VALUE).required,
+    value: Joi.string().pattern(REGEX_VALUE).required(),
+    amount: Joi.string().pattern(REGEX_VALUE).required(),
 }).required();
 
 module.exports = {
