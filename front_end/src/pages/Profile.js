5ce20b62095e03005256fce40356c01a76a1ded2
Blucilha <blucilha.nascimento@gmail.com>
2021-10-26 18:58:22 -0300

    adiciona css login e tabela

--- front_end/src/pages/Profile.js
+++ front_end/src/pages/Profile.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import '../styles/Profile.css'
 import Header from '../components/Header';
 
 function Profile() {
@@ -16,13 +17,15 @@ function Profile() {
     return (
         <div>
             <Header infor={ infor } />
-            <h3>{ infor.name }</h3>
-            <section>
-                <p>{ infor.id }</p>
-                <p>{ infor.doc }</p>
-                <p>{ infor.site }</p>
-                <p>{ infor.active }</p>
-                <p>{ infor.about }</p>
+            <section className='session'>
+                <div className='card-profile'>
+                    <h3>{ infor.name }</h3>
+                    <p><strong>ID:</strong>{ ' ' }{ infor.id }</p>
+                    <p><strong>CNPJ:</strong>{ ' ' }{ infor.doc }</p>
+                    <p><strong>SITE:</strong>{ ' ' }{ infor.site }</p>
+                    <p><strong>ATIVO:</strong>{ ' ' }{ infor.active.toString() }</p>
+                    <p><strong>DESCRIÇÃO:</strong>{ ' ' }{ infor.about }</p>
+                </div>
             </section>
         </div>
     )
